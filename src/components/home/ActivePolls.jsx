e1705a662a2681c7bb8ff73abb4474785097eee0
Danko Novovic <danko.novovic@gmail.com>
2020-05-21 14:34:17 +0200

    activeOrders in progress

--- src/components/home/ActivePolls.jsx
+++ src/components/home/ActivePolls.jsx
@@ -4,6 +4,8 @@ class ActivePolls extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            //userName treba da bude username ulogovanog usera sa local storage
+            userName: 'Dusan Bobicic',
             allPolls: [
                 {
                     pollId: 0,
@@ -57,10 +59,61 @@ class ActivePolls extends React.Component {
         // this.setState({ allPolls: allPolls });
     }
 
+    deletePoll = (index) => {
+        let activePollsAfterDelete = this.state.allPolls;
+        activePollsAfterDelete.splice(index, 1);
+        this.setState({ allPolls: activePollsAfterDelete });
+    }
+
+
     render(){
+
+
+        let allPolls = this.state.allPolls;
+        let pollsRow = [];
+        if(allPolls.length>0) {
+            allPolls.map((poll, index)=>{
+                if(poll.author==this.state.userName){
+                    pollsRow.push(
+                        <tr>
+                            <td>{poll.name}</td>
+                            <td>{poll.author}</td>
+                            <td>{poll.ends}</td>
+                            <td><button>Vote</button>
+                                <button>End</button>
+                                <button onClick={()=>this.deletePoll(index)}>Delete</button>
+                            </td>               
+                        </tr>);
+                } else {
+                    pollsRow.push(
+                        <tr>
+                            <td>{poll.name}</td>
+                            <td>{poll.author}</td>
+                            <td>{poll.ends}</td>
+                            <td><button>Vote!</button></td>                
+                        </tr>);
+                }
+                
+            })
+        } else {
+            pollsRow = <tr><td>No active polls</td></tr>;
+        }
+        
+
+        
         return (
             <div>
-                <button>Vote!</button>
+                <h2>ACTIVE POLLS</h2>
+                <table>                 
+                    <tr>
+                        <th>Name</th>
+                        <th>Author</th>
+                        <th>End time</th>
+                        <th>Actions</th>
+                    </tr>
+                    {pollsRow}
+                </table>
+                <button>Create new poll</button>      
             </div>
         )
     }
