e75d382a3461a66d5999512e8853634a005e4c77
Danko Novovic <danko.novovic91@gmail.com>
2020-05-23 23:06:37 +0200

    minor change

--- src/components/Order/ComboOrderList.jsx
+++ src/components/Order/ComboOrderList.jsx
@@ -1,55 +1,60 @@
 import React, { useState } from "react";
 import { appStorage } from "../../services/storage.service";
 
-const ComboOrderList = ({meals, orderId, orderedMeals, setOrderedMeals, total, setTotal,addOrderItems}) => {
+const ComboOrderList = ({
+  meals,
+  orderId,
+  orderedMeals,
+  setOrderedMeals,
+  total,
+  setTotal,
+  addOrderItems,
+}) => {
+  const saltyMeals = meals.filter((meal) => meal.tags.includes("#slano"));
+  const sweetMeals = meals.filter((meal) => meal.tags.includes("#slatko"));
+  const [budget, setBudget] = useState(0);
 
-    const saltyMeals = meals.filter(meal=> meal.tags.includes("#slano"));
-    const sweetMeals = meals.filter(meal=> meal.tags.includes("#slatko"));
-    const [budget, setBudget] = useState(0);
-    
-
-    let comboRow = [];
-    saltyMeals.forEach((saltyMeal) => {
-      sweetMeals.forEach((sweetMeal) => {
-        if (saltyMeal.price + sweetMeal.price <= budget) {
-          comboRow.push(
-            <div style={{display:'flex'}}>             
-              <div>{saltyMeal.name}</div>
-              <div>{sweetMeal.name}</div>
-              <div>
-                {saltyMeal.price + sweetMeal.price}
-              </div>
-              <div>
-                <button
-                  onClick={() => {addComboItems(saltyMeal,sweetMeal)}}
-                >
-                  Add combo
-                </button>
-              </div>
+  let comboRow = [];
+  saltyMeals.forEach((saltyMeal) => {
+    sweetMeals.forEach((sweetMeal) => {
+      if (saltyMeal.price + sweetMeal.price <= budget) {
+        comboRow.push(
+          <div style={{ display: "flex" }}>
+            <div>{saltyMeal.name}</div>
+            <div>{sweetMeal.name}</div>
+            <div>{saltyMeal.price + sweetMeal.price}</div>
+            <div>
+              <button
+                onClick={() => {
+                  addComboItems(saltyMeal, sweetMeal);
+                }}
+              >
+                Add combo
+              </button>
             </div>
-          );
-        }
-      });
+          </div>
+        );
+      }
     });
+  });
 
-    const addComboItems = (saltyMeal,sweetMeal) => {
-        addOrderItems([saltyMeal,sweetMeal]);
-    };
+  const addComboItems = (saltyMeal, sweetMeal) => {
+    addOrderItems([saltyMeal, sweetMeal]);
+  };
 
-    return(
-        <div>
-            <h2>Get your perfect combo</h2>
-            <input type="text" onChange={(e) => setBudget(e.target.value)} />       
-            <div style={{display: "flex"}}>
-                
-                <div>Meal</div>
-                <div>Dessert</div>
-                <div>Price</div>
-                <div>Actions</div>
-            </div>
-                <div>{comboRow}</div> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>Get your perfect combo</h3>
+      <input type="text" onChange={(e) => setBudget(e.target.value)} />
+      <div style={{ display: "flex" }}>
+        <div>Meal</div>
+        <div>Dessert</div>
+        <div>Price</div>
+        <div>Actions</div>
+      </div>
+      <div>{comboRow}</div>
+    </div>
+  );
+};
 
-export default ComboOrderList
+export default ComboOrderList;
--- src/components/home/ActivePolls.jsx
+++ src/components/home/ActivePolls.jsx
@@ -1,131 +1,152 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import { polls } from '../../../src/data';
-
+import { polls } from "../../../src/data";
+import { appStorage } from "../../services/storage.service";
 
 class ActivePolls extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            //userName treba da bude username ulogovanog usera sa local storage
-            userName: 'Pera',
-            allPolls: polls                      
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: appStorage.getUser(),
+      allPolls: polls,
+    };
+  }
 
+  componentDidMount() {
+    this.setAllPolls();
+  }
 
-    
+  setAllPolls = async () => {
+    // let allPolls = await getAllPolls();
+    //getAllPolls ce biti u service i vuci ce sve polove sa servera
+    // this.setState({ allPolls: allPolls });
+  };
 
-    componentDidMount() {
-        this.setAllPolls();
-    }
+  //brisace poll sa servera kada bude gotov back
+  //sada brise samo iz state-a
+  deletePoll = (index) => {
+    let activePollsAfterDelete = this.state.allPolls;
+    activePollsAfterDelete.splice(index, 1);
+    this.setState({ allPolls: activePollsAfterDelete });
+  };
 
-    setAllPolls = async () => {
-        // let allPolls = await getAllPolls();
-        //getAllPolls ce biti u service i vuci ce sve polove sa servera
-        // this.setState({ allPolls: allPolls });
-    }
+  endPoll = (pollId) => {
+    //salje pobednicki restoran u niz ordera na backu
+    //refreshuje home stranicu, kako bismo na njoj odmah videli novi order
+  };
 
-    //brisace poll sa servera kada bude gotov back
-    //sada brise samo iz state-a
-    deletePoll = (index) => {
-        let activePollsAfterDelete = this.state.allPolls;
-        activePollsAfterDelete.splice(index, 1);
-        this.setState({ allPolls: activePollsAfterDelete });
-    }
-
-    endPoll = (pollId) => {
-        //salje pobednicki restoran u niz ordera na backu 
-        //refreshuje home stranicu, kako bismo na njoj odmah videli novi order
-    }
-
-
-    render(){
-        let allPolls = this.state.allPolls;
-        let pollsRow = [];
-        if(allPolls.length>0) {
-            allPolls.map((poll, index)=>{
-                if(poll.author===this.state.userName){
-                    pollsRow.push(
-                        <div className="active-info">
-                        <div>
-                            <label className='pollLblInfo'> {poll.name}</label>
-                        </div>
-                        <div>
-                            <label className='pollLblInfo'>{poll.author}</label>
-                        </div>
-                        <div>
-                            <label className='pollLblInfo'>{poll.ends}</label>
-                        </div>
-                        <div className="btn-icons">
-                            <div>
-                                <img src="./img/del.png" alt="icon" title="Delete" onClick={()=>this.deletePoll(index)}/>
-                            </div>
-                            <div>
-                                <img src="./img/end1.png" alt="icon" title="End Poll"/>
-                            </div>
-                            <div>
-                              <Link to={`/poll/${poll.pollId}`} className='voteBtnLink'><img src="./img/vote1.png" alt="icon" title="Vote" /></Link>  
-                            </div>
-                        </div>
-                    </div>);
-                } else {
-                    pollsRow.push(
-                        <div className="active-info">
-                        <div>
-                            <label className='pollLblInfo'>{poll.name}</label>
-                        </div>
-                        <div>
-                            <label  className='pollLblInfo'>{poll.author}</label>
-                        </div>
-                        <div>
-                            <label  className='pollLblInfo'>{poll.ends}</label>
-                        </div>
-                        <div className="className='pollGuest'">
-                            <div >
-                              <Link to={`/poll/${poll.pollId}`} className='voteBtnLink'><img src="./img/vote1.png" alt="icon" title="Vote" className='pollGuestIcon'/></Link>  
-                            </div>
-                        </div>
-                    </div>);
-                }              
-            })
-        } else {
-            pollsRow =   <div className="active-info"><label >No Active Polls</label></div>;
-        }
-                
-        return (
-            <div className="active-polls">
-            <div className="poll-order-card">
-                <img className="pollCardicon" src="./img/pollcard4.png" alt='pollicon' />
-                <div className="card-heading">
-                    <h1>Active Polls</h1>
+  render() {
+    let allPolls = this.state.allPolls;
+    let pollsRow = [];
+    if (allPolls.length > 0) {
+      allPolls.map((poll, index) => {
+        if (poll.author === this.state.userName) {
+          pollsRow.push(
+            <div className="active-info">
+              <div>
+                <label className="pollLblInfo"> {poll.name}</label>
+              </div>
+              <div>
+                <label className="pollLblInfo">{poll.author}</label>
+              </div>
+              <div>
+                <label className="pollLblInfo">{poll.ends}</label>
+              </div>
+              <div className="btn-icons">
+                <div>
+                  <img
+                    src="./img/del.png"
+                    alt="icon"
+                    title="Delete"
+                    onClick={() => this.deletePoll(index)}
+                  />
                 </div>
-
-                <div className="active-info header">
-                    <div>
-                        <label  >Name</label>
-                    </div>
-                    <div>
-                        <label>Author</label>
-                    </div>
-                    <div>
-                        <label>Remaining Time</label>
-                    </div>
-                    <div>
-                        <label>Action</label>
-                    </div>
+                <div>
+                  <img src="./img/end1.png" alt="icon" title="End Poll" />
                 </div>
-{pollsRow}
-              
-                <div className="card-btn-wrapper">
-                    <button className="btn-green"><Link to={'/createpoll'} className='creBtnLink'>Create New Poll</Link></button>
+                <div>
+                  <Link to={`/poll/${poll.pollId}`} className="voteBtnLink">
+                    <img src="./img/vote1.png" alt="icon" title="Vote" />
+                  </Link>
                 </div>
-
+              </div>
             </div>
-
+          );
+        } else {
+          pollsRow.push(
+            <div className="active-info">
+              <div>
+                <label className="pollLblInfo">{poll.name}</label>
+              </div>
+              <div>
+                <label className="pollLblInfo">{poll.author}</label>
+              </div>
+              <div>
+                <label className="pollLblInfo">{poll.ends}</label>
+              </div>
+              <div className="className='pollGuest'">
+                <div>
+                  <Link to={`/poll/${poll.pollId}`} className="voteBtnLink">
+                    <img
+                      src="./img/vote1.png"
+                      alt="icon"
+                      title="Vote"
+                      className="pollGuestIcon"
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        }
+      });
+    } else {
+      pollsRow = (
+        <div className="active-info">
+          <label>No Active Polls</label>
         </div>
-        )
+      );
     }
+
+    return (
+      <div className="active-polls">
+        <div className="poll-order-card">
+          <img
+            className="pollCardicon"
+            src="./img/pollcard4.png"
+            alt="pollicon"
+          />
+          <div className="card-heading">
+            <h1>Active Polls</h1>
+          </div>
+
+          <div className="active-info header">
+            <div>
+              <label>Name</label>
+            </div>
+            <div>
+              <label>Author</label>
+            </div>
+            <div>
+              <label>Remaining Time</label>
+            </div>
+            <div>
+              <label>Action</label>
+            </div>
+          </div>
+          {pollsRow}
+
+          <div className="card-btn-wrapper">
+            <button className="btn-green">
+              <Link to={"/createpoll"} className="creBtnLink">
+                Create New Poll
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ActivePolls;
