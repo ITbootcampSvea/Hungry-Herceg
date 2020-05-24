d5d241edbb7879f3eef4e5490f588151146e8295
mmplanic <mmplanic@gmail.com>
2020-05-24 21:53:17 +0200

    AdminRoutes and guards implemented

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import CreatePoll from "./components/CreatePoll/CreatePoll";
 import Vote from "./components/Vote/Vote";
 import Profile from "./components/Profile/Profile";
 import Settings from "./components/Settings/Settings";
+import AdminRoute from "./routes/AdminRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+
         <PublicRoute component={LogIn } path="/login" />
 
         <PrivateRoute component={Home} path="/home" />
@@ -22,11 +24,10 @@ function App() {
         <PrivateRoute component={CreatePoll} path="/createpoll" />
         <PrivateRoute component={Vote} path="/vote/:id" />
         {/* <PrivateRoute component={Statistics} path="/stats" /> */}
-        <PrivateRoute component={Settings} path="/settings" />
         <PrivateRoute component={Profile} path="/profile" />
 
-        {/* <PrivateRoute component={Poll} path="/poll/:id" /> */}
-        {/* <PrivateRoute component={Order} path="/order/:id" /> */}
+        <AdminRoute component={Settings} path="/settings" />
+
 
         <Redirect to={authService.isLoged() ? "/home" : "/login"} />
       </Switch>
