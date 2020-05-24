af598adaad40ba1633a281ebb1d5d931813c82d6
Danko Novovic <danko.novovic91@gmail.com>
2020-05-24 14:19:57 +0200

    change

--- src/components/Order/ComboOrderList.jsx
+++ src/components/Order/ComboOrderList.jsx
@@ -4,12 +4,7 @@ import './Order.css'
 
 const ComboOrderList = ({
   meals,
-  orderId,
-  orderedMeals,
-  setOrderedMeals,
-  total,
-  setTotal,
-  addOrderItems,
+  addOrderItems
 }) => {
   const saltyMeals = meals.filter((meal) => meal.tags.includes("#slano"));
   const sweetMeals = meals.filter((meal) => meal.tags.includes("#slatko"));
