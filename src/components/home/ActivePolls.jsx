8fc72c7e822105b886ae4252c6c804698da11309
Danko Novovic <danko.novovic91@gmail.com>
2020-05-26 11:07:48 +0200

    fixed link to vote

--- src/components/home/ActivePolls.jsx
+++ src/components/home/ActivePolls.jsx
@@ -79,7 +79,7 @@ class ActivePolls extends React.Component {
                     />
                   </div>
                   <div>
-                    <Link to={`/vote/:${poll._id}`} className="voteBtnLink">
+                    <Link to={`/vote/${poll._id}`} className="voteBtnLink">
                       <img src="./img/vote1.png" alt="icon" title="Vote" />
                     </Link>
                   </div>
@@ -100,7 +100,7 @@ class ActivePolls extends React.Component {
                 </div>
                 <div className="className='pollGuest'">
                   <div>
-                    <Link to={`/vote/:${poll._id}`} className="voteBtnLink">
+                    <Link to={`/vote/${poll._id}`} className="voteBtnLink">
                       <img
                         src="./img/vote1.png"
                         alt="icon"
